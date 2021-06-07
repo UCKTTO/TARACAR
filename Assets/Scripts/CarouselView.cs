65f0ccd22a98b8373f3cbdf8a75c7e1f1378c976
Wyli Olivete <azeptoblade@gmail.com>
2021-06-07 17:28:03 +0800

    init explore, tourist

---
+++ Assets/Scripts/CarouselView.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CarouselView : MonoBehaviour {
+
+
+    public RectTransform[] images;
+    public RectTransform view_window;
+
+    private bool  canSwipe;
+    private float image_width;
+    private float lerpTimer;
+    private float lerpPosition;
+    private float mousePositionStartX;
+    private float mousePositionEndX;
+    private float dragAmount;
+    private float screenPosition;
+    private float lastScreenPosition;
+    /// <summary>
+    /// Space between images.
+    /// </summary>
+    private  float image_gap;
+
+    public int swipeThrustHold          = 30;
+    [HideInInspector]
+    /// <summary>
+    /// The index of the current image on display.
+    /// </summary>
+    public int current_index;
+
+    public Text titel;
+
+    #region mono
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(InitUI());
+    }
+
+    private IEnumerator InitUI() {
+        yield return new WaitForEndOfFrame();
+
+        image_width = view_window.rect.width/3;
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].sizeDelta = new Vector2(view_window.rect.width/3, view_window.rect.height);
+            if (i >= 1) {
+                images[i].anchoredPosition = new Vector2(((image_width + image_gap) * i), 0);
+            }
+        };
+        image_gap = image_width * 0.25f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // titel.text = current_index.ToString();
+
+        lerpTimer = lerpTimer + Time.deltaTime;
+
+        if (lerpTimer < 0.333f)
+        {
+            screenPosition = Mathf.Lerp(lastScreenPosition, lerpPosition * -1, lerpTimer * 3);
+            lastScreenPosition = screenPosition;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            canSwipe = true;
+            mousePositionStartX = Input.mousePosition.x;
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            if (canSwipe)
+            {
+                mousePositionEndX = Input.mousePosition.x;
+                dragAmount = mousePositionEndX - mousePositionStartX;
+                screenPosition = lastScreenPosition + dragAmount;
+            }
+        }
+
+        if (Mathf.Abs(dragAmount) > swipeThrustHold && canSwipe)
+        {
+            canSwipe = false;
+            lastScreenPosition = screenPosition;
+            if (current_index < images.Length)
+                OnSwipeComplete();
+            else if (current_index == images.Length && dragAmount < 0)
+                lerpTimer = 0;
+            else if (current_index == images.Length && dragAmount > 0)
+                OnSwipeComplete();
+        }
+
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].sizeDelta = new Vector2(view_window.rect.width/3 * 0.75f, view_window.rect.height * 0.75f);
+            images[i].anchoredPosition = new Vector2(screenPosition + ((image_width + image_gap) * i), 0);
+        }
+        images[current_index].sizeDelta = new Vector2(view_window.rect.width/3 * 1.75f, view_window.rect.height * 1.25f);
+    }
+    #endregion
+
+
+    #region private methods
+    void OnSwipeComplete()
+    {
+        lastScreenPosition = screenPosition;
+
+        if (dragAmount > 0)
+        {
+            if (dragAmount >= swipeThrustHold)
+            {
+                if (current_index == 0)
+                {
+                    lerpTimer = 0; lerpPosition = 0;
+                }
+                else
+                {
+                    current_index--;
+                    lerpTimer = 0;
+                    if (current_index < 0)
+                        current_index = 0;
+                    lerpPosition = (image_width + image_gap) * current_index;
+                }
+            }
+            else
+            {
+                lerpTimer = 0;
+            }
+        }
+        else if (dragAmount < 0)
+        {
+            if (Mathf.Abs(dragAmount) >= swipeThrustHold)
+            {
+                if (current_index == images.Length-1)
+                {
+                    lerpTimer = 0;
+                    lerpPosition = (image_width + image_gap) * current_index;
+                }
+                else
+                {
+                    lerpTimer = 0;
+                    current_index++;
+                    lerpPosition = (image_width + image_gap) * current_index;
+                }
+            }
+            else
+            {
+                lerpTimer = 0;
+            }
+        }
+        dragAmount = 0;
+    }
+    #endregion
+
+
+
+    #region public methods
+    public void GoToIndex(int value)
+    {
+        current_index = value;
+        lerpTimer = 0;
+        lerpPosition = (image_width + image_gap) * current_index;
+        screenPosition = lerpPosition * -1;
+        lastScreenPosition = screenPosition;
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].anchoredPosition = new Vector2(screenPosition + ((image_width + image_gap) * i), 0);
+        }
+    }
+
+    public void GoToIndexSmooth(int value)
+    {
+        current_index = value;
+        lerpTimer = 0;
+        lerpPosition = (image_width + image_gap) * current_index;
+    }
+    #endregion
+}
